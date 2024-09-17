cd1170af2eada3ca4d6bf1c04e91798e558651a9
lokeshdhakar97 <lokeshdhakar1404@gmail.com>
2024-09-17 16:34:42 +0530

    add color

--- src/components/Buttons.tsx
+++ src/components/Buttons.tsx
@@ -7,7 +7,7 @@ export interface MyButtonsProps {
 export default function MyButtons(data: MyButtonsProps) {
   return (
     <button
-      className={`px-6 md:px-8 rounded-lg py-3  w-fit text-white font-semibold text-base md:text-lg bg-${data.bgColor} ${data.btnStyles}
+      className={`px-6 md:px-8 rounded-lg py-3  w-fit text-white  font-semibold text-base md:text-lg bg-${data.bgColor} ${data.btnStyles}
       `}
     >
       {data.title}
