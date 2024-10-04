4ce5d26d7bb27f76e1f3775bec38b8ea80a4ae54
lokeshdhakar97 <lokeshdhakar1404@gmail.com>
2024-10-04 11:29:07 +0530

    update footer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -73,15 +73,7 @@ export default function Home() {
         </div>
       </div>
 
-      <div className="h-fit w-full ">
-        <svg
-          className="inline-block fill-current w-full h-auto text-primaryBlue"
-          viewBox="0 0 1440 450"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <path d="M1189.2 169.2H421H253.8C159.8 169.2 69.1 203.1 0 262.6V449.8C30.5 349.9 131.3 276.7 252 276.7H424.1H1187.4C1280.9 276.7 1371 243.2 1440 184.3V0C1408.1 97.9 1308.3 169.2 1189.2 169.2Z"></path>
-        </svg>
-      </div>
+      
     </div>
   )
 }
